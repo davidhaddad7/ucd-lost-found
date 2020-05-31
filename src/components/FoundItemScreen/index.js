c6ecc61b7c6e4820769cda5d0f09c6635134a5af
George Revkov <compresce@gmail.com>
2020-05-30 18:25:43 -0700

    Finished implementing mobile version of UI for Found Item Screen'

--- src/components/FoundItemScreen/index.js
+++ src/components/FoundItemScreen/index.js
@@ -1,30 +1,44 @@
 import React, { Component } from 'react';
-import { PageHeader, Card, InputField, Button, InputFile, COLORS } from '..';
 import './styles.css';
+import {
+  Header,
+  Card,
+  InputField,
+  FakeSearchField,
+  InputTextArea,
+  Button,
+  InputFile,
+  COLORS
+} from '..';
+
 
 class FoundItemScreen extends Component {
 
   render() {
     return (
-      <div>
-        <PageHeader text="Input the found item" />
         <main>
-          <Card>
-            <InputField label="Title" />
-            <InputField label="Category" />
-            <InputField label="Description" height="20vh" />
-            <InputFile label="Attach a photo (optional)" />
-            <div className="line-aligned-right">
-              <Button
-                text="Next"
-                onClick={() => console.log("Clicked")}
-                bgColor={COLORS.darkOrange}
-                textColor={COLORS.white}
-              />
-            </div>
-          </Card>
+          <section>
+            <Header text="Input the found item" />
+            <Card>
+              <InputField label="Title" />
+              <InputField label="Category" />
+              <InputTextArea label="Description" />
+              <InputFile label="Attach a photo (optional)" />
+              <div className="line-aligned-right">
+                <Button
+                  text="Next"
+                  onClick={() => console.log("Clicked")}
+                  bgColor={COLORS.darkOrange}
+                  textColor={COLORS.white}
+                />
+              </div>
+            </Card>
+          </section>
+          <section>
+            <Header text="Or search for existing requests" />
+            <FakeSearchField />
+          </section>
         </main>
-      </div>
     );
   }
 }
