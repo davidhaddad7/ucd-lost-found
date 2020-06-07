95079e664c3730a6db3117feb1ec052dd0703541
davidhaddad7 <davhaddad@ucdavis.edu>
2020-06-06 22:09:03 -0700

    making axios post request to send item

--- src/containers/FoundItemScreenContainer.js
+++ src/containers/FoundItemScreenContainer.js
@@ -1,6 +1,23 @@
 import React, { Component } from 'react';
 import { FoundItemScreen } from '../components';
 import { Colors, ThemeContext } from '../lib';
+import axios from 'axios';
+
+async function onNextButton() {
+  try {
+    const token = await axios.post('/post-found-item', {
+      newFoundItem:'found item beats'
+    })
+  } catch (err) {
+    throw new Error('Unable to make post new found item')
+  }
+  //send us to next page
+}
+
+// createNewFountItem(){
+
+// }
+
 
 class FoundItemScreenContainer extends Component {
   render() {
