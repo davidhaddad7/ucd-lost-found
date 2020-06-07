d69ef6b388a9cedca4191a7e8311744c28b7f4fe
davidhaddad7 <davhaddad@ucdavis.edu>
2020-06-06 21:38:06 -0700

    getting local state

--- src/components/FoundItemScreen/index.js
+++ src/components/FoundItemScreen/index.js
@@ -17,6 +17,30 @@ class FoundItemScreen extends Component {
   constructor(props) {
     super(props);
     props.setBgColor(Colors.lightOrange);
+
+    this.state = {
+                  title: '',
+                  category:'',
+                  description:''
+                 };
+
+    this.handleTitleChange = this.handleTitleChange.bind(this);
+    this.handleCategoryChange = this.handleCategoryChange.bind(this);
+    this.handleDescriptionChange = this.handleTitleChange.bind(this);
+  }
+
+  handleTitleChange(event) {
+    this.setState({title: event.target.value});
+    console.log("title: ",this.state.title);
+  }
+  handleCategoryChange(event) {
+    this.setState({category: event.target.value});
+    console.log("category: ",this.state.category);
+
+  }
+  handleDescriptionChange(event) {
+    this.setState({description: event.target.value});
+    console.log("description: ",this.state.description);
   }
 
   buttonStyle = {
@@ -30,9 +54,17 @@ class FoundItemScreen extends Component {
           <section>
             <Header text="Input the found item" />
             <Card>
-              <InputField label="Title" />
-              <InputField label="Category" />
-              <InputTextArea label="Description" />
+
+              {/* Title */}
+              <InputField label="Title" type="text" value={this.state.title} onChange={this.handleTitleChange} />
+
+              {/* Category */}
+              <InputField label="Category" type="text" value={this.state.category} onChange={this.handleCategoryChange}/>
+
+              {/* Description */}
+              <InputTextArea label="Description" type="text" value={this.state.description} onChange={this.handleDescriptionChange} />
+
+              {/* Image */}
               <InputFile label="Attach a photo (optional)" />
               <div className="found-item-screen-line-aligned-right">
                 <Button
--- src/components/InputField/index.js
+++ src/components/InputField/index.js
@@ -4,10 +4,12 @@ import './styles.css';
 export const InputField = (props) => {
   const height = props.height || 'auto';
 
+  const { onChange } = props;
+
   return (
     <div className="input-field-container">
       <div className="input-field-label" >{props.label}</div>
-      <input className="input-field" type='text' style={{ height}} />
+      <input className="input-field" type='text' onChange={onChange} style={{ height}} />
     </div>
   )
 }
