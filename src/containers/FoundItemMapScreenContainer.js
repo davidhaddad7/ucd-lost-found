cd51cba8eebda2358d704a72da7a960c88d4428d
George Revkov <compresce@gmail.com>
2020-06-07 18:45:53 -0700

    FoundItemMapScreenContainer

--- src/containers/FoundItemMapScreenContainer.js
+++ src/containers/FoundItemMapScreenContainer.js
@@ -3,8 +3,33 @@ import { FoundItemMapScreen } from '../components';
 import { Colors, ThemeContext } from '../lib';
 
 class FoundItemMapScreenContainer extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      creationDate: new Date(),
+      creationTime: new Date()
+    }
+  }
+
+  onCreationDateChange = (newDate) => {
+    this.setState({ creationDate: newDate })
+  }
+
+  onCreationTimeChange = (newTime) => {
+    this.setState({ creationTime: newTime })
+  }
+
   render() {
-    return <FoundItemMapScreen setBgColor={this.context.changeThemeColor} />;
+    return (
+      <FoundItemMapScreen
+        date={this.state.creationDate}
+        time={this.state.creationTime}
+        onDateChange={this.onCreationDateChange}
+        onTimeChange={this.onCreationTimeChange}
+        setBgColor={this.context.changeThemeColor}
+      />
+    );
   }
 }
 
