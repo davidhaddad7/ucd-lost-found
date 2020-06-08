b1ff605a88af494c169f61f2e9225d51859adf2d
davidhaddad7 <davhaddad@ucdavis.edu>
2020-06-07 17:08:30 -0700

    time picker for found item map

--- src/components/FoundItemMapScreen/index.js
+++ src/components/FoundItemMapScreen/index.js
@@ -7,13 +7,32 @@ import {
   FakeSearchField
 } from '..';
 import { SearchFieldMapContainer } from '../../containers';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 class FoundItemMapScreen extends Component {
 
   constructor(props) {
     super(props);
     props.setBgColor(Colors.lightOrange);
+    this.state = {
+      Date: new Date(),
+      hourDate: new Date(),
+      location: null
+    };
   }
+ 
+  setDate = date => {
+    this.setState({
+      Date: date
+    });
+  };
+
+  setHourDate = date => {
+    this.setState({
+      hourDate: date
+    });
+  };
 
   buttonStyle = {
     backgroundColor: Colors.darkOrange,
@@ -26,6 +45,26 @@ class FoundItemMapScreen extends Component {
           <section>
             <Header text="Input the found item" />
             <Card>
+              <div className = "date-container">
+                <div className = "month-day-year-container">
+                  <DatePicker 
+                  selected={this.state.Date} 
+                  onChange={date => this.setDate(date)} 
+                  />
+                </div>
+                <div className = "hour-minute-container">
+                  <DatePicker
+                      selected={this.state.hourDate}
+                      onChange={date => this.setHourDate(date)}
+                      showTimeSelect
+                      showTimeSelectOnly
+                      timeIntervals={15}
+                      timeCaption="Time"
+                      dateFormat="h:mm aa"
+                  />
+                </div>
+              </div>
+            
               <SearchFieldMapContainer />
             </Card>
           </section>
